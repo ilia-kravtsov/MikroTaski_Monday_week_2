ed4db1dee15acae492fe2cdcb629f66b938f201c
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2023-01-17 13:46:36 +0400

    17.01.23 Start

---
+++ mikrotaski_m_w_2/src/App.tsx
@@ -0,0 +1,30 @@
+import React, {useState} from 'react';
+import logo from './logo.svg';
+import './App.css';
+function App() {
+        const [message, setMessage] = useState([
+              {message: 'message1'},
+              {message: 'message2'},
+              {message: 'message3'},
+              {message: 'message4'},
+              {message: 'message5'}
+            ]
+        )
+        return (
+            <div className="App">
+              <div>
+                <input />
+                <button>+</button>
+              </div>
+              {message.map((el, index) => {
+                return (
+                    <div key={index}>{el.message}</div>
+                )
+              })}
+            </div>
+        );
+      }
+
+
+
+  export default App;
